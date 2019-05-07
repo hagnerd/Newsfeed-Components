919da799afe1b7d5adf0835ecdf4421a9b250a95
Matt Hagner <web@matthagner.com>
2019-05-07 15:22:13 -0500

    Complete article requirements

--- Article/Article.js
+++ Article/Article.js
@@ -1,6 +1,6 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
-class Article {
+class Article { 
   constructor(domElement) {
     // assign this.domElement to the passed in domElement
     this.domElement = domElement;
@@ -9,12 +9,12 @@ class Article {
     // Using your expandButton reference, update the text on your expandButton to say "expand"
     this.expandButton.textContent = "expand"; 
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () => expandArticle()); 
+    this.expandButton.addEventListener('click', () => this.expandArticle()); 
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.domElement.classList.toggle('article-open')
   }
 }
 
